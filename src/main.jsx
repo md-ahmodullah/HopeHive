e59d999a6b08de0ca9cf3259694895d81b5d5761
md-ahmodullah <contactme.ahmod@gmail.com>
2024-12-04 02:27:04 +0600

    Installed npm packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="text-4xl text-red-600">
+        Hello world! <button className="btn btn-accent">Accent</button>
+      </div>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
