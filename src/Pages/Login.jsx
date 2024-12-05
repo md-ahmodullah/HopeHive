7f213eeb6fae7196936274b11c0a38b580dbf09d
md-ahmodullah <contactme.ahmod@gmail.com>
2024-12-06 02:49:10 +0600

    Make Fancy Login-Register Page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -57,105 +57,112 @@ export default function Login() {
   };
   return (
     <>
-      <div className="my-6 py-5 lg:py-16 font-poppins">
-        <div className="card bg-base-200 w-full md:w-1/2 lg:w-1/3 mx-auto shrink-0 shadow-2xl px-6">
-          <div className="text-center px-5 py-3">
-            <h1 className="text-2xl font-bold text-deepTeal">Login</h1>
-          </div>
-          <div className="px-2 py-3 space-y-2">
-            <form onSubmit={handleSubmit}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Email</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                  >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                  </svg>
-                  <input
-                    type="email"
-                    name="email"
-                    className="grow"
-                    placeholder="Email"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Password</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    type={isShow ? "text" : "password"}
-                    name="password"
-                    className="grow"
-                    placeholder="password"
-                    required
-                  />
-                  {isShow ? (
-                    <FaEyeSlash className="text-lg" onClick={handleShow} />
-                  ) : (
-                    <FaEye className="text-lg" onClick={handleShow} />
-                  )}
-                </label>
-                <label className="label">
-                  <Link
-                    to="/reset-password"
-                    href="#"
-                    className="label-text-alt link link-hover"
-                  >
-                    Forgot password?
-                  </Link>
-                </label>
-              </div>
-              <div className="flex w-full flex-col border-opacity-50 form-control mt-2">
-                <div>
-                  {errMessage && (
-                    <span className="text-base text-red-500 flex items-center gap-1 pb-3">
-                      <IoWarning className="text-xl" /> {errMessage}
+      <div className="bg-[url('https://i.ibb.co.com/WzQqTmX/bg-addnew.png')] bg-blue-700 bg-blend-multiply bg-no-repeat bg-cover bg-center min-h-[600px] font-poppins">
+        <div className="my-6 py-5 lg:py-16 font-poppins">
+          <div className="bg-blue-600 bg-opacity-20 backdrop-blur-sm rounded-lg border border-gray-400 w-full md:w-1/2 lg:w-1/3 mx-auto shrink-0 shadow-2xl px-6">
+            <div className="text-center px-5 py-3">
+              <h1 className="text-2xl font-semibold text-yellow-300">
+                Login Your Account
+              </h1>
+            </div>
+            <div className="px-2 py-3 space-y-2">
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-semibold text-white text-lg">
+                      Email
                     </span>
-                  )}
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70 text-white"
+                    >
+                      <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                      <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input
+                      type="email"
+                      name="email"
+                      className="grow"
+                      placeholder="Email"
+                      required
+                    />
+                  </label>
                 </div>
-                <button className="btn bg-blue-600 font-bold text-white">
-                  Login
-                </button>
-                <div className="divider">OR</div>
-                <button
-                  className="btn btn-outline mb-3 text-white bg-blue-600"
-                  onClick={handleGoogleLogin}
-                >
-                  <FaGoogle /> Sign In with Google
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className="flex items-center justify-center text-xs pb-6 font-medium">
-            <p className="pr-2">Don't Have An Account?</p>
-            <Link
-              to="/auth/register"
-              className="text-blue-600 font-bold underline"
-            >
-              Register
-            </Link>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-semibold text-white text-lg">
+                      Password
+                    </span>
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70 text-white"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <input
+                      type={isShow ? "text" : "password"}
+                      name="password"
+                      className="grow"
+                      placeholder="password"
+                      required
+                    />
+                    {isShow ? (
+                      <FaEyeSlash
+                        className="text-lg text-gray-300"
+                        onClick={handleShow}
+                      />
+                    ) : (
+                      <FaEye
+                        className="text-lg text-gray-300"
+                        onClick={handleShow}
+                      />
+                    )}
+                  </label>
+                </div>
+                <div className="flex w-full flex-col border-opacity-50 form-control mt-2">
+                  <div>
+                    {errMessage && (
+                      <span className="text-base text-red-500 flex items-center gap-1 pb-3">
+                        <IoWarning className="text-xl" /> {errMessage}
+                      </span>
+                    )}
+                  </div>
+                  <button className="btn btn-outline font-bold text-yellow-300 hover:btn-warning hover:text-black">
+                    Login
+                  </button>
+                  <div className="divider text-white before:bg-gray-400 after:bg-gray-400">
+                    OR
+                  </div>
+                  <button
+                    className="btn btn-outline mb-3 text-yellow-300 hover:btn-warning hover:text-black"
+                    onClick={handleGoogleLogin}
+                  >
+                    <FaGoogle /> Sign In with Google
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="flex items-center justify-center text-xs pb-6 font-medium">
+              <p className="pr-2 text-gray-200">Don't Have An Account?</p>
+              <Link
+                to="/auth/register"
+                className="text-yellow-300 font-medium underline"
+              >
+                Register
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -69,143 +69,154 @@ export default function Register() {
   };
   return (
     <>
-      <div className="mb-6 py-5 lg:py-4 font-poppins">
-        <div className="card bg-base-200 w-full md:w-1/2 lg:w-1/3 mx-auto shrink-0 shadow-2xl px-5">
-          <div className="text-center px-5 pt-3">
-            <h1 className="text-2xl font-bold text-deepTeal">Register</h1>
-          </div>
-          <div className="px-5 py-1">
-            <form onSubmit={handleSubmit}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Name</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <MdDriveFileRenameOutline />
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Mr Thingumbob"
-                    className="grow"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Photo URL</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <FaLink className="text-sm text-gray-500" />
-                  <input
-                    type="text"
-                    name="photo"
-                    placeholder="https://google.com/mr/thingumbob.jpg"
-                    className="grow"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Email</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                  >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                  </svg>
-                  <input
-                    type="email"
-                    name="email"
-                    className="grow"
-                    placeholder="mrthingumbob@gmail.com"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-semibold">Password</span>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    type={isShow ? "text" : "password"}
-                    name="password"
-                    className="grow"
-                    placeholder="Z^a6vf8P#"
-                    required
-                  />
-                  {isShow ? (
-                    <FaEyeSlash
-                      className="text-xl md:text-lg"
-                      onClick={handleShow}
-                    />
-                  ) : (
-                    <FaEye
-                      className="text-xl md:text:lg"
-                      onClick={handleShow}
-                    />
-                  )}
-                </label>
-              </div>
-              <div className="form-control inline-block space-x-2 pt-2">
-                <input
-                  type="checkbox"
-                  name="terms"
-                  className="checkbox"
-                  required
-                />
-                <span className="text-sm text-gray-500">
-                  Agree to the Terms and Conditions
-                </span>
-              </div>
-              <div className="flex w-full flex-col border-opacity-50 form-control mt-3">
-                <div>
-                  {errMessage && (
-                    <span className="text-xs text-red-500 flex items-center gap-1 pb-3">
-                      <IoWarning className="text-2xl" /> {errMessage}
+      <div className="bg-[url('https://i.ibb.co.com/WzQqTmX/bg-addnew.png')] bg-blue-700 bg-blend-multiply bg-no-repeat bg-cover bg-center min-h-[500px] font-poppins">
+        <div className="py-5 lg:py-4 font-poppins">
+          <div className="bg-blue-600 bg-opacity-20 backdrop-blur-sm rounded-lg border border-gray-400 w-full md:w-1/2 lg:w-1/3 mx-auto shrink-0 shadow-2xl px-5">
+            <div className="text-center px-5 pt-3">
+              <h1 className="text-2xl font-semibold text-yellow-300">
+                Register Your Account
+              </h1>
+            </div>
+            <div className="px-5 py-1">
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium text-base text-white">
+                      Name
                     </span>
-                  )}
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <MdDriveFileRenameOutline className="text-gray-300" />
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Mr Thingumbob"
+                      className="grow"
+                      required
+                    />
+                  </label>
                 </div>
-                <button className="btn bg-blue-600 font-semibold text-white">
-                  Register
-                </button>
-                <div className="divider">OR</div>
-                <button
-                  onClick={handleGoogleSignup}
-                  className="btn btn-outline mb-3 text-white bg-blue-600"
-                >
-                  <FaGoogle /> Sign Up with Google
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className="flex items-center justify-center text-xs pb-3 font-semibold">
-            <p className="pr-2">Already have an account?</p>
-            <Link
-              to="/auth/login"
-              className="text-blue-600 font-bold underline"
-            >
-              Log In
-            </Link>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium text-base text-white">
+                      Photo URL
+                    </span>
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <FaLink className="text-sm text-gray-300" />
+                    <input
+                      type="text"
+                      name="photo"
+                      placeholder="https://google.com/mr/thingumbob.jpg"
+                      className="grow"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium text-base text-white">
+                      Email
+                    </span>
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70 text-white"
+                    >
+                      <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                      <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input
+                      type="email"
+                      name="email"
+                      className="grow"
+                      placeholder="mrthingumbob@gmail.com"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium text-base text-white">
+                      Password
+                    </span>
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 bg-transparent border border-gray-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70 text-white"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <input
+                      type={isShow ? "text" : "password"}
+                      name="password"
+                      className="grow"
+                      placeholder="Z^a6vf8P#"
+                      required
+                    />
+                    {isShow ? (
+                      <FaEyeSlash
+                        className="text-xl md:text-lg text-gray-300"
+                        onClick={handleShow}
+                      />
+                    ) : (
+                      <FaEye
+                        className="text-xl md:text:lg text-gray-300"
+                        onClick={handleShow}
+                      />
+                    )}
+                  </label>
+                </div>
+                <div className="form-control inline-block space-x-2 pt-2">
+                  <input
+                    type="checkbox"
+                    name="terms"
+                    className="checkbox-warning"
+                    required
+                  />
+                  <span className="text-sm text-gray-300">
+                    Agree to the Terms and Conditions
+                  </span>
+                </div>
+                <div className="flex w-full flex-col border-opacity-50 form-control mt-3">
+                  <div>
+                    {errMessage && (
+                      <span className="text-xs text-red-500 flex items-center gap-1 pb-3">
+                        <IoWarning className="text-2xl" /> {errMessage}
+                      </span>
+                    )}
+                  </div>
+                  <button className="btn btn-outline font-semibold text-yellow-300 hover:btn-warning hover:text-black">
+                    Register
+                  </button>
+                  <div className="divider text-white before:bg-gray-400 after:bg-gray-400">
+                    OR
+                  </div>
+                  <button
+                    onClick={handleGoogleSignup}
+                    className="btn btn-outline mb-3 text-yellow-300 hover:btn-warning hover:text-black"
+                  >
+                    <FaGoogle /> Sign Up with Google
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="flex items-center justify-center text-xs pb-3 font-medium">
+              <p className="pr-2 text-gray-200">Already have an account?</p>
+              <Link to="/auth/login" className="text-yellow-300 underline">
+                Log In
+              </Link>
+            </div>
           </div>
         </div>
       </div>
